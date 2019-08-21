cd1bcdf5663e5c9e7a3dc2799008cb01afcce990
Dylan Palmieri <dgpalmieri@gmail.com>
2019-08-21 13:04:57 -0800

    Move functionality to better align with planning.md

--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 
 import sys
 import pygame
-import os
 import block, ball, scoreboard
 
 
@@ -23,6 +22,7 @@ def playGame():
 
         movePaddles()
         moveBall()
+        updateDisplay()
 
 
 def startScreen():
@@ -31,7 +31,7 @@ def startScreen():
         # display splash screen
 
         movePaddles()
-
+        updateDisplay()
         # if spacebar is pressed
         break
 
@@ -58,28 +58,22 @@ def moveBall():
 
 
 def updateDisplay():
-    pass
-
-
-def main():
-    pygame.init()
-
-    current_path = os.path.dirname(__file__)
-    images_path = os.path.join(current_path, "images/")
 
     size = width, height = 800, 600
     black = 0, 0, 0
     pygame.display.set_caption("Pong?")
 
     screen = pygame.display.set_mode(size)
-    paddle = pygame.image.load(os.path.join(images_path, "paddle.png"))
-    paddleRect = paddle.get_rect()
+
+    screen.fill(black)
+    pygame.display.flip()
+
+
+def main():
+    pygame.init()
 
     startScreen()
     playGame()
-    screen.fill(black)
-    screen.blit(paddle, paddleRect)
-    pygame.display.flip()
 
 
 if __name__ == "__main__":
