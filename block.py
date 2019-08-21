8cd091e75d896bd4b0325fd23cf64098df50f29c
Dylan Palmieri <dgpalmieri@gmail.com>
2019-08-21 10:29:25 -0800

    Add dummy Block children classes

---
+++ block.py
@@ -0,0 +1,20 @@
+# block.py
+# Dylan Palmieri
+# 9/19/19
+# Contains Block parent class
+
+
+class Block:
+    pass
+
+
+class Wall(Block):
+    pass
+
+
+class Paddle(Block):
+    pass
+
+
+class Background(Block):
+    pass
